3d953bfb512927aabcbb70fcc567e0a90e61217d
Zagadochnik37 <Elephant-5@yandex.ru>
2020-07-08 16:10:21 +0300

    Этап 1 - готово

--- app/src/main/java/com/mss/homework1/Adapter.kt
+++ app/src/main/java/com/mss/homework1/Adapter.kt
@@ -7,13 +7,14 @@ import androidx.recyclerview.widget.RecyclerView
 class Adapter(private val data: List<Any>) :
     RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
-    override fun getItemViewType(position: Int): Int = position
-        /*when (position) {
-            0 -> 0
-            1 -> 1
-            2 -> 2
-            3 -> 3
-        }*/
+    override fun getItemViewType(position: Int): Int =
+        when (data[position]) {
+            is User -> 0
+            is Project -> 1
+            is SkillsHeader -> 2
+            is Skill -> 3
+            else -> throw Exception("Wrong type")
+        }
         //return super.getItemViewType(position)
 
 
@@ -22,6 +23,8 @@ class Adapter(private val data: List<Any>) :
         return when (viewType) {
             0 -> UserHolder(inflater.inflate(R.layout.user_info, parent, false))
             1 -> ProjectHolder(inflater.inflate(R.layout.project_info, parent, false))
+            2 -> SkillsHeaderHolder(inflater.inflate(R.layout.skills_header, parent, false))
+            3 -> SkillHolder(inflater.inflate(R.layout.skill_item, parent, false))
             else -> throw Exception("Wrong viewType")
         }
     }
@@ -30,6 +33,7 @@ class Adapter(private val data: List<Any>) :
         when (getItemViewType(position)) {
             0 -> (holder as UserHolder).bind(data[position] as User)
             1 -> (holder as ProjectHolder).bind(data[position] as Project)
+            3 -> (holder as SkillHolder).bind(data[position] as Skill)
         }
     }
 
