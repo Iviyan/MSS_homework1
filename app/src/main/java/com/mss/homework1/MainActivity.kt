50480f0229887a129456c399d9f8fc5394a993df
Zagadochnik37 <Elephant-5@yandex.ru>
2020-07-08 16:44:14 +0300

    ktlintFormat

--- app/src/main/java/com/mss/homework1/MainActivity.kt
+++ app/src/main/java/com/mss/homework1/MainActivity.kt
@@ -1,9 +1,9 @@
 package com.mss.homework1
 
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
